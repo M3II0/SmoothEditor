f2a8946ebae67ed0e0e1fc304ef472645be940ed
mello <mellomainmail@gmail.com>
2023-08-10 17:16:43 +0200

    - Added cmd

--- src/main/java/sk/m3ii0/smootheditor/code/readers/ItemReader.java
+++ src/main/java/sk/m3ii0/smootheditor/code/readers/ItemReader.java
@@ -12,17 +12,35 @@ import sk.m3ii0.smootheditor.code.editor.enums.MenuAction;
 import sk.m3ii0.smootheditor.code.readers.utils.XMaterial;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 public class ItemReader {
 	
+	private final static Method SET_CUSTOM_MODEL_DATA;
+	
+	static {
+		Method SET_CUSTOM_MODEL_DATA1;
+		try {
+			SET_CUSTOM_MODEL_DATA1 = ItemMeta.class.getDeclaredMethod("setCustomModelData", Integer.class);
+		} catch (NoSuchMethodException e) {
+			SET_CUSTOM_MODEL_DATA1 = null;
+		}
+		SET_CUSTOM_MODEL_DATA = SET_CUSTOM_MODEL_DATA1;
+		if (SET_CUSTOM_MODEL_DATA != null) {
+			SET_CUSTOM_MODEL_DATA.setAccessible(true);
+		}
+	}
+	
 	public static ItemStack read(char id) {
 		String path = "Items." + id + ".";
 		FileConfiguration c = SmoothEditor.getOptions();
 		String color = c.getString(path + "Color");
 		int amount = c.getInt(path + "Amount");
+		int cmd = c.getInt(path + "CustomModelData");
 		String name = c.getString(path + "Name");
 		List<String> lore = c.getStringList(path + "Lore");
 		lore.replaceAll(var -> var.replace("&", "§"));
@@ -32,6 +50,13 @@ public class ItemReader {
 			item = createSkull(texture);
 			SkullMeta meta = (SkullMeta) item.getItemMeta();
 			item.setAmount(amount);
+			if (hasModelData()) {
+				try {
+					SET_CUSTOM_MODEL_DATA.invoke(meta, cmd);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					throw new RuntimeException(e);
+				}
+			}
 			meta.setDisplayName(name.replace("&", "§"));
 			meta.setLore(lore);
 			item.setItemMeta(meta);
@@ -41,6 +66,13 @@ public class ItemReader {
 		item = XMaterial.requestXMaterial(c.getString(path + "Material"), getColor(color)).parseItem();
 		item.setAmount(amount);
 		ItemMeta meta = item.getItemMeta();
+		if (hasModelData()) {
+			try {
+				SET_CUSTOM_MODEL_DATA.invoke(meta, cmd);
+			} catch (IllegalAccessException | InvocationTargetException e) {
+				throw new RuntimeException(e);
+			}
+		}
 		meta.setDisplayName(name.replace("&", "§"));
 		meta.setLore(lore);
 		item.setItemMeta(meta);
@@ -130,6 +162,10 @@ public class ItemReader {
 		}
 		
 		return head;
+	}
+	
+	private static boolean hasModelData() {
+		return SET_CUSTOM_MODEL_DATA != null;
 	}
 	
 }
